e9b6d49fee383eee48cfd8285764b602fbf649aa
Pablo Bozzolo <boctulus@gmail.com>
2021-08-24 00:27:47 -0300

    .

--- sse.php
+++ sse.php
@@ -1,45 +1,13 @@
 <?php
 
-class SSE
-{
-	private $channel;
-	private $interval = 1;
-
-	function __construct(){
-		header("Content-Type: text/event-stream\n\n");
-	}
-
-	function setDefaultChannel(string $name){
-		// validar. Solo permitidos [a-z-#] y alguno más. Ej: vendor-x#created
-		$this->channel = $name; 
-	}
-
-	function setInterval(int $seconds){
-		$this->interval = $seconds;
-	}
-
-	function send($data, string $channel = null){
-		if (empty($channel)){
-			$channel = !empty($this->channel) ? $this->channel : 'default';
-		}
-
-		echo "event: $channel\n";
-		echo 'data: ' . json_encode($data);
-		echo "\n\n";
-
-		ob_flush();
-		flush();
-
-		sleep($this->interval);
-	}
-
-}
+require 'Sse.php';
 
 /*
 	Ejemplo de uso
 */
 
 $sse = new SSE();
+$sse->setInterval(1);
 
 while (1) 
 {
@@ -48,7 +16,9 @@ while (1)
 		'age'  => 10
 	];
 
+	$sse->send('Hello World');  
 	$sse->send($data, 'vendor-x#created');
+	
 }
 
 
